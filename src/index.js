6831b12d8cadc174182cbfbc59c1dd51c8fafafe
MonicahMackena¨ <mackenamuthui@gmail.com>
2025-01-16 09:07:42 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import ReactDOM from 'react-dom/client';
 import Keypad from './components/Keypad';
 import EyesOnMe from './components/EyesOnMe';
 
-ReactDOM.render(
-  <div>
-    <Keypad />
-    <EyesOnMe />
-  </div>,
-  document.getElementById('root')
-);
+
+function App(){
+  return(
+    <div>
+      <h1>Enter Your Password</h1>
+      <Keypad />
+      <EyesOnMe/>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
